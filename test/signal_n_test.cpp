436c0aa8185e7990d415816b2361123ed91699e6
Frank Mori Hess <fmh6jj@gmail.com>
2007-03-13 17:38:37 +0000

    We can't explicitly track signals, except in the normal way with shared_ptr.
    [SVN r3777]

--- test/signal_n_test.cpp
+++ test/signal_n_test.cpp
@@ -149,7 +149,7 @@ test_signal_signal_connect()
 
   {
     signal_type s2;
-    s1.connect(signal_type::slot_type(s2).track(s2));
+    s1.connect(signal_type::slot_type(s2));
     s2.connect(std::bind1st(std::multiplies<int>(), 2));
     s2.connect(std::bind1st(std::multiplies<int>(), -3));
 
