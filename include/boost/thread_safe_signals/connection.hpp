48e789142bfbb34e800155ef01aec14bcbb35d03
Frank Mori Hess <fmh6jj@gmail.com>
2007-09-26 20:30:35 +0000

    Removed US gov copyright related notices, since this no longer involves just US gov employees.
    [SVN r39558]

--- include/boost/thread_safe_signals/connection.hpp
+++ include/boost/thread_safe_signals/connection.hpp
@@ -3,16 +3,6 @@
   Author: Frank Hess <frank.hess@nist.gov>
   Begin: 2007-01-23
 */
-/* This software was developed at the National Institute of Standards and
- * Technology by employees of the Federal Government in the course of
- * their official duties. Pursuant to title 17 Section 105 of the United
- * States Code this software is not subject to copyright protection and is
- * in the public domain. This is an experimental system. NIST assumes no
- * responsibility whatsoever for its use by other parties, and makes no
- * guarantees, expressed or implied, about its quality, reliability, or
- * any other characteristic. We would appreciate acknowledgement if the
- * software is used.
- */
 // Use, modification and
 // distribution is subject to the Boost Software License, Version
 // 1.0. (See accompanying file LICENSE_1_0.txt or copy at
