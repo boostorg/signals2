4f5e801a41f21e14721361f3378f4c0f90ea2348
Frank Mori Hess <fmh6jj@gmail.com>
2008-09-24 14:48:26 +0000

    Fixed up header comments
    [SVN r48945]

--- test/deconstruct_test.cpp
+++ test/deconstruct_test.cpp
@@ -1,11 +1,11 @@
-// Boost.Signals library
+// Tests for boost::signals2::deconstruct_ptr and friends
 
-// Copyright (C) Douglas Gregor 2001-2006. Use, modification and
-// distribution is subject to the Boost Software License, Version
+// Copyright Frank Mori Hess 2007-2008.
+// Distributed under the Boost Software License, Version
 // 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-// For more information, see http://www.boost.org
+// See http://www.boost.org/libs/signals2 for library home page.
 
 #include <boost/shared_ptr.hpp>
 #include <boost/test/minimal.hpp>
