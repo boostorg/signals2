3e255edc25213739118f21c4f37700f5d005b73e
Frank Mori Hess <fmh6jj@gmail.com>
2008-10-07 14:20:07 +0000

    Replaced some tab indentation with spaces.
    [SVN r49163]

--- include/boost/signals2/detail/signals_common_macros.hpp
+++ include/boost/signals2/detail/signals_common_macros.hpp
@@ -19,39 +19,39 @@
 #define BOOST_SIGNAL_SIGNATURE_ARG_NAME(z, n, data) BOOST_PP_CAT(arg, BOOST_PP_INC(n))
 // Tn argn
 #define BOOST_SIGNAL_SIGNATURE_FULL_ARG(z, n, data) \
-	BOOST_PP_CAT(T, BOOST_PP_INC(n)) BOOST_SIGNAL_SIGNATURE_ARG_NAME(~, n, ~)
+  BOOST_PP_CAT(T, BOOST_PP_INC(n)) BOOST_SIGNAL_SIGNATURE_ARG_NAME(~, n, ~)
 // T1 arg1, T2 arg2, ..., Tn argn
 #define BOOST_SIGNAL_SIGNATURE_FULL_ARGS(arity) \
-	BOOST_PP_ENUM(arity, BOOST_SIGNAL_SIGNATURE_FULL_ARG, ~)
+  BOOST_PP_ENUM(arity, BOOST_SIGNAL_SIGNATURE_FULL_ARG, ~)
 // arg1, arg2, ..., argn
 #define BOOST_SIGNAL_SIGNATURE_ARG_NAMES(arity) BOOST_PP_ENUM(arity, BOOST_SIGNAL_SIGNATURE_ARG_NAME, ~)
 // typename prefixR, typename prefixT1, typename prefixT2, ..., typename prefixTN
 #define BOOST_SIGNAL_PREFIXED_SIGNATURE_TEMPLATE_DECL(arity, prefix) \
-	typename BOOST_PP_CAT(prefix, R) BOOST_PP_COMMA_IF(arity) \
-	BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_INC(arity), typename BOOST_PP_CAT(prefix, T))
+  typename BOOST_PP_CAT(prefix, R) BOOST_PP_COMMA_IF(arity) \
+  BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_INC(arity), typename BOOST_PP_CAT(prefix, T))
 // typename R, typename T1, typename T2, ..., typename TN
 #define BOOST_SIGNAL_SIGNATURE_TEMPLATE_DECL(arity) BOOST_SIGNAL_PREFIXED_SIGNATURE_TEMPLATE_DECL(arity,)
 // prefixR, prefixT1, prefixT2, ..., prefixTN
 #define BOOST_SIGNAL_PREFIXED_SIGNATURE_TEMPLATE_INSTANTIATION(arity, prefix) \
-	BOOST_PP_CAT(prefix, R) BOOST_PP_COMMA_IF(arity) BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_INC(arity), BOOST_PP_CAT(prefix, T))
+  BOOST_PP_CAT(prefix, R) BOOST_PP_COMMA_IF(arity) BOOST_PP_ENUM_SHIFTED_PARAMS(BOOST_PP_INC(arity), BOOST_PP_CAT(prefix, T))
 // R, T1, T2, ..., TN
 #define BOOST_SIGNAL_SIGNATURE_TEMPLATE_INSTANTIATION(arity) \
-	BOOST_SIGNAL_PREFIXED_SIGNATURE_TEMPLATE_INSTANTIATION(arity,)
+  BOOST_SIGNAL_PREFIXED_SIGNATURE_TEMPLATE_INSTANTIATION(arity,)
 // functionN<R, T1, T2, ..., TN>
 #define BOOST_FUNCTION_N_DECL(arity) BOOST_PP_CAT(function, arity)<\
-	BOOST_SIGNAL_SIGNATURE_TEMPLATE_INSTANTIATION(arity) >
+  BOOST_SIGNAL_SIGNATURE_TEMPLATE_INSTANTIATION(arity) >
 // slotN
 #define BOOST_SLOT_CLASS_NAME(arity) BOOST_PP_CAT(slot, arity)
 // typename function_traits<Signature>::argn_type
 #define BOOST_SIGNAL_SIGNATURE_TO_ARGN_TYPE(z, n, Signature) \
-	BOOST_PP_CAT(BOOST_PP_CAT(typename function_traits<Signature>::arg, BOOST_PP_INC(n)), _type)
+  BOOST_PP_CAT(BOOST_PP_CAT(typename function_traits<Signature>::arg, BOOST_PP_INC(n)), _type)
 // typename function_traits<Signature>::result_type,
 // typename function_traits<Signature>::arg1_type,
 // typename function_traits<Signature>::arg2_type,
 // ...,
 // typename function_traits<Signature>::argn_type
 #define BOOST_SIGNAL_PORTABLE_SIGNATURE(arity, Signature) \
-	typename function_traits<Signature>::result_type \
-	BOOST_PP_COMMA_IF(arity) BOOST_PP_ENUM(arity, BOOST_SIGNAL_SIGNATURE_TO_ARGN_TYPE, Signature)
+  typename function_traits<Signature>::result_type \
+  BOOST_PP_COMMA_IF(arity) BOOST_PP_ENUM(arity, BOOST_SIGNAL_SIGNATURE_TO_ARGN_TYPE, Signature)
 
 #endif	// BOOST_SIGNALS_COMMON_MACROS_HEADER
